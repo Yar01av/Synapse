8b0a773f72de6402b88b986acfbc11342fe8d984
Yar0s1av <yaroslavn99@gmail.com>
2020-10-31 13:05:58 +0100

    Refactoring

--- agents/a2c/standard.py
+++ agents/a2c/standard.py
@@ -89,53 +89,56 @@ class A2C(AgentTraining):
             if transition.done:
                 episode_idx += 1
 
-                new_rewards = steps_generator.pop_per_episode_rewards()
-                assert len(new_rewards) == 1
-                last_episodes_rewards.extend(new_rewards)
-                self._plotter.add_scalar("Total reward per episode", new_rewards[0], episode_idx)
-                print(f"At step {idx}, \t the average over the last 100 games is "
-                      f"{sum(last_episodes_rewards)/min(len(last_episodes_rewards), 100)}")
+                self._log_new_rewards(episode_idx, idx, last_episodes_rewards, steps_generator)
 
             if len(self._memory) == self._batch_size:
-
-                t_states, t_actions, t_qvals = unpack(self._memory,
-                                                      self._model,
-                                                      self._gamma,
-                                                      self._unfolding_steps,
-                                                      "cuda")
-
-                self._optimizer.zero_grad()
-                t_logits, t_values = self._model(t_states)
-                t_log_probs = t_logits.log_softmax(dim=1)
-                t_probs = t_logits.softmax(dim=1)
-
-                # Compute the value loss
-                value_loss = F.mse_loss(t_values.squeeze(-1), t_qvals)
-
-                # Compute the policy loss
-                t_advantages = t_qvals - t_values.detach()
-                policy_loss = -(t_advantages*t_log_probs[range(self._batch_size), t_actions]).mean()
-
-                # Compute the entropy and record the original probabilities for later
-                entropy = -(t_probs*t_log_probs).sum(dim=1).mean()
-                old_probs = t_probs
-
-                (policy_loss+value_loss-self._beta*entropy).backward()
-                nn_utils.clip_grad_norm_(self._model.parameters(), self._clip_grad)
-                self._optimizer.step()
-
-                # Compute KL divergence
-                new_probs = self._model(t_states)[0].softmax(dim=1)
-                kl_divergence = -((new_probs/old_probs).log()*old_probs).sum(dim=1).mean()
-
-                self._memory.clear()
-
-                # Plot
-                self._plotter.add_scalar("Combined Loss", (policy_loss+value_loss-self._beta*entropy).item(), idx)
-                self._plotter.add_scalar("Entropy", entropy.item(), idx)
-                self._plotter.add_scalar("KL Divergence", kl_divergence.item(), idx)
+                self._learn(idx)
 
         self._plotter.close()
+
+    def _log_new_rewards(self, episode_idx, idx, last_episodes_rewards, steps_generator):
+        new_rewards = steps_generator.pop_per_episode_rewards()
+        assert len(new_rewards) == 1
+        last_episodes_rewards.extend(new_rewards)
+
+        self._plotter.add_scalar("Total reward per episode", new_rewards[0], episode_idx)
+        print(f"At step {idx}, \t the average over the last 100 games is "
+              f"{sum(last_episodes_rewards) / min(len(last_episodes_rewards), 100)}")
+
+    def _learn(self, idx):
+        t_states, t_actions, t_qvals = unpack(self._memory,
+                                              self._model,
+                                              self._gamma,
+                                              self._unfolding_steps,
+                                              "cuda")
+        self._optimizer.zero_grad()
+        t_logits, t_values = self._model(t_states)
+        t_log_probs = t_logits.log_softmax(dim=1)
+        t_probs = t_logits.softmax(dim=1)
+
+        # Compute the value loss
+        value_loss = F.mse_loss(t_values.squeeze(-1), t_qvals)
+
+        # Compute the policy loss
+        t_advantages = t_qvals - t_values.detach()
+        policy_loss = -(t_advantages * t_log_probs[range(self._batch_size), t_actions]).mean()
+
+        # Compute the entropy and record the original probabilities for later
+        entropy = -(t_probs * t_log_probs).sum(dim=1).mean()
+        old_probs = t_probs
+        (policy_loss + value_loss - self._beta * entropy).backward()
+        nn_utils.clip_grad_norm_(self._model.parameters(), self._clip_grad)
+        self._optimizer.step()
+
+        # Compute KL divergence
+        new_probs = self._model(t_states)[0].softmax(dim=1)
+        kl_divergence = -((new_probs / old_probs).log() * old_probs).sum(dim=1).mean()
+        self._memory.clear()
+
+        # Plot
+        self._plotter.add_scalar("Combined Loss", (policy_loss + value_loss - self._beta * entropy).item(), idx)
+        self._plotter.add_scalar("Entropy", entropy.item(), idx)
+        self._plotter.add_scalar("KL Divergence", kl_divergence.item(), idx)
 
     @classmethod
     def load_selector(cls, load_path) -> BaseActionSelector:
